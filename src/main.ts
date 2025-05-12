90807f263da8efdaab65d705e614ede09baa619c
Mukut Prasad <mukutprasad1996@gmail.com>
2025-05-12 11:22:33 +0530

    Cors error

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './appModule';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001','http://13.201.80.65:3000/'],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://13.201.80.65:3000',
+        'http://13.201.80.65:3001',
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS policy violation'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
-  await app.listen(process.env.PORT);
+
+  await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap();
