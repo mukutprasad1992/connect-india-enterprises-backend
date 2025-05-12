1b4f79df3f5b750ea8095d056fa4328758836918
Rajiv Kumar Gupta <156166934+kumarrajiv0918@users.noreply.github.com>
2025-05-12 10:57:28 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './appModule';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001','http://13.201.80.65:3000/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
