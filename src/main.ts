a5e79233835e01f7f6c0898c37e424f9c33fc071
Mukut Prasad <mukutprasad1996@gmail.com>
2026-02-20 15:52:04 +0530

    Modification on main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './appModule';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://3.237.2.74:3001',
-        'http://localhost:3000',
-        'http://localhost:54099'
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('CORS policy violation'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: [
+      'http://3.237.2.74:3001',
+      'http://localhost:3000',
+      'http://localhost:54099',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
 }
 
 bootstrap();
